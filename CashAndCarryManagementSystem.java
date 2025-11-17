6404a5141cf82d938298fb21fb47f5c1dc62669a
Jawad Khan <byteofjawad@gmail.com>
2025-11-17 19:24:48 +0500

    Your message here

--- CashAndCarryManagementSystem.java
+++ CashAndCarryManagementSystem.java
@@ -1,11 +1,39 @@
 public class CashAndCarryManagementSystem{
-	public static void main(String[] args) {
-	
-	int a = 3;
-	int b = 4;
-	int sum = 0;
-	sum = a + b;
-	System.out.println(sum);
-}
-
+	private static void mainMenu() {
+        while (true) {
+            System.out.println("\n" + "=".repeat(80));
+            System.out.println("                   CASH & CARRY MANAGEMENT SYSTEM");
+            System.out.println("                           MAIN MENU");
+            System.out.println("=".repeat(80));
+            System.out.println("1. Inventory Management");
+            System.out.println("2. Customer Management");
+            System.out.println("3. Point of Sale (POS)");
+            System.out.println("4. Reports");
+            System.out.println("5. Exit");
+            System.out.println("=".repeat(80));
+            System.out.print("Select an option: ");
+            
+            int choice = getIntInput();
+            
+            switch (choice) {
+                case 1:
+                    inventoryMenu();
+                    break;
+                case 2:
+                    customerMenu();
+                    break;
+                case 3:
+                    posMenu();
+                    break;
+                case 4:
+                    reportsMenu();
+                    break;
+                case 5:
+                    System.out.println("\nThank you for using Cash & Carry Management System!");
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
 }
